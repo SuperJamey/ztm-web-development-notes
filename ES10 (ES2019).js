5e2c20661bc2aa063fa0cf2b4767ca5e9152e146
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2022-12-30 13:54:59 -0600

    add completed Exercise: ES10
    Exorcise or quiz from the ES10 module
    problems + solutions

--- ES10 (ES2019).js
+++ ES10 (ES2019).js
@@ -7,8 +7,8 @@ concatenates all the elements of the given multidimensional array,
 and flats upto the specified depth. We can specify the depth limit 
 to where we need to flatten the array. By default, the depth limit is 1 */
 
-const array = [1,2,3,4,5]
-array.flat()  // output = [1,2,3,4,5] - this array is already flat.
+const array1 = [1,2,3,4,5]
+array1.flat()  // output = [1,2,3,4,5] - this array is already flat.
 
 const array2 = [1,[2,3],[4,5]]
 array2.flat()  // output = [1,2,3,4,5] - no more nested arrays, array is now flat.
@@ -71,4 +71,50 @@ Object.entries(obj1)  // output - [Array(2), Array(2), Array(2)]
  // } catch {
  // if an error occured, run this
  // }
- 
+
+
+ // Exercise: ES10
+
+ // Solve the below questions:
+
+// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+const array = [[1],[2],[3],[[[4]]],[[[5]]]]
+// solution
+console.log(array.flat(2))  // output = [1,2,3,[4],[5]]
+
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+// solution
+console.log(greeting.flatMap(x => x.join(' ')))
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+// solution
+console.log(greeting.flatMap(x => x.join(' ')).join(' '))
+
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+// solution
+console.log(trapped.flat(50))
+
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+// solution
+console.log(userEmail3.trimStart())
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+// solution
+const usersArray = Object.entries(users)
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+// solution
+updatedUsersArray = usersArray.map(user => [user[0], user[1] * 2])
+
+//#8 change the output array of  #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+// solution
+const updatedUsers = Object.fromEntries(updatedUsersArray)
+console.log(updatedUsers)
